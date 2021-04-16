d011a6b0074a49ba4ff6ce3d7239190975bec1d5
Connor <thecmac17@gmail.com>
2021-04-15 23:26:18 -0400

    Editing GeneratedBlockContainer.tsx to crop images that are too large.

--- src/GeneratedBlockContainer.tsx
+++ src/GeneratedBlockContainer.tsx
@@ -9,11 +9,13 @@ interface generatedBlockContainerProps {
 export default function GeneratedBlockContainer({texturePack, generatedBlock}: generatedBlockContainerProps){
     return(
         <div className="Image-container">
-            <img
-                src={`${process.env.PUBLIC_URL}/texture-pack/${texturePack}/assets/minecraft/textures/block/${generatedBlock.image}`}
-                className="App-image"
-                alt={generatedBlock.blockName}
-            />
+            <div className="Image-boundary">
+                <img
+                    src={`${process.env.PUBLIC_URL}/texture-pack/${texturePack}/assets/minecraft/textures/block/${generatedBlock.image}`}
+                    className="App-image"
+                    alt={generatedBlock.blockName}
+                />
+            </div>
             <p className="block-name">{generatedBlock.blockName}</p>
             <p className="block-id" data-tip='copy' onClick={() => {window.clientInformation.clipboard.writeText(generatedBlock.blockId).then()}}>
                 {generatedBlock.blockId}
